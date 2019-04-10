e804d49abe3fe516342f7c2e69b3914ffa15687a
rajeshn <rajeshn@ngen.it>
2019-04-10 14:57:29 -0500

    Changed to use pm2 to start/stop

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World");
+  res.send("Hello World v1.0");
 });
 
 app.listen(4000, function () {
